90c0908d36af0a101f9f2ff5f01ce0677c47116a
prasanjeevi <prasanjeevi@gmail.com>
2024-06-26 11:16:20 +0530

    mitreattack-python basic integration

---
+++ lambda_function.py
@@ -0,0 +1,6 @@
+from mitreattack.stix20 import MitreAttackData
+mitre_attack_data = MitreAttackData("enterprise-attack.json")
+
+def lambda_handler(event, context):
+    tactics_map = mitre_attack_data.get_tactics_by_matrix()
+    return [{"name": t['name'], "description": t['description'], "id": t['external_references'][0]['external_id']} for t in tactics_map['Enterprise ATT&CK']]
